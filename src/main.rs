f622d51c7fdc3ed2f33b9ffc063fa66383b718b8
Tobias Karius <tobias.karius@hm.edu>
2021-02-08 16:06:30 +0100

    implemented hardcoded version of new-nonce

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,17 @@ const SERVER: &str = "https://acme-staging-v02.api.letsencrypt.org/directory";
 #[allow(dead_code)]
 const KEY_WIDTH: u32 = 2048;
 
+const NONCE: &str = "https://acme-staging-v02.api.letsencrypt.org/acme/new-nonce";
+
 fn main() {
     let client = Client::new();
 
     //get the JSONs from get_directory in String form
     get_directory(&client);
 
-    
+    let response = send_get_new_nonce(&client).unwrap();
+    println!("test");
+    println!("{}", response);
 
     //parse the 
 
@@ -44,6 +48,11 @@ fn get_directory(client: &Client) {
 fn send_get_directory_request(client: &Client) -> Result<String, Error> {
     let url = Url::parse(SERVER)?;
     Ok(client.get(url).send()?.text()?)
+}
+
+fn send_get_new_nonce(client: &Client) -> Option<String> {
+    let url = Url::parse(NONCE).unwrap();
+    Some(client.head(url).send().unwrap().headers().get("replay-nonce").unwrap().to_str().unwrap().to_owned())
 }
 
 #[allow(dead_code)]
