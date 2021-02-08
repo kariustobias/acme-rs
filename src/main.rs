1adc909b1b0632485202b38a8d58af41fd421a37
Bastian Kersting <bastian@cmbt.de>
2021-02-08 13:39:07 +0100

    reqwest get demo and correct cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+/// enum Option<T> {
+///     Some(T),
+///     None    
+// }
+
+/// enum Result<T, E> {
+///     Ok(T),
+///     Err(E)    
+// }
+
+use reqwest::blocking::Client;
+use reqwest::Url;
+
+const SERVER: &str = "http://google.de";
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+    println!("{}", send_get_request(&client).unwrap());
+
+    println!("{}", send_get_request(&client).unwrap());
 }
+
+fn send_get_request(client: &Client) -> Result<String, reqwest::Error> {
+    if let Ok(url) = Url::parse(SERVER) {
+        return client.get(url).send()?.text();
+    }
+    Ok(String::from("-Hat ned geklappt"))
+}
+
