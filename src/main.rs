9399db008cabd048fb1dbbc7e7b5d28aa039e28a
Tobias Karius <tobias.karius@hm.edu>
2021-02-08 14:13:31 +0100

    request sent to lets-encrypt

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,31 @@ mod error;
 use reqwest::blocking::Client;
 use reqwest::Url;
 
-const SERVER: &str = "https://google.de";
+const SERVER: &str = "https://acme-staging-v02.api.letsencrypt.org/directory";
 
 fn main() {
     let client = Client::new();
-    println!("{}", send_get_request(&client).unwrap());
+
+    //get the JSONs from get_directory in String form
+    get_directory(&client);
+
+    
+
+    //parse the 
+
+    //println!("{}", get_directory(&client).unwrap());
 }
 
-fn send_get_request(client: &Client) -> Result<String, error::Error> {
+fn get_directory(client: &Client) {
+    //perform get request to initial URL
+    let response = send_get_directory_request(&client).unwrap();
+    println!("{}", response);
+    //deserialize the JSON String
+
+    //store the important URLs
+}
+
+fn send_get_directory_request(client: &Client) -> Result<String, error::Error> {
     let url = Url::parse(SERVER)?;
     Ok(client.get(url).send()?.text()?)
 }
